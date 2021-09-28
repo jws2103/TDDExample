7ecda041b39102bc20bbe72c76ba204209acb382
Jay <jay.sim@xam-consulting.com>
2021-09-28 23:24:33 +1000

    Added the comments with the requirements

--- TDDExample/Services/BankAccountService.cs
+++ TDDExample/Services/BankAccountService.cs
@@ -6,21 +6,38 @@ namespace TDDExample
         private BankAccount _currentAccount;
         public BankAccount CreateAccount(string accountHolder, double initAmount)
         {
+            /* Requirements
+                1.1  Throw the exception if account holder is null or empty string
+                1.2. Throw the exception if initAmount is less than 0
+                2.  return BankAccount object with the fake account & bsb number
+            */
             throw new NotImplementedException();
         }
 
         public double GetBalance()
         {
+            /* Requirements
+                1.  Throw the exception if your current account doesn't exist
+                2.  Return the balance of your current account
+             */
             throw new NotImplementedException();
         }
 
         public void Deposit(double amount)
         {
+            /* Requirements
+                1.  Throw the exception if your current account doesn't exist
+                2.  Add your balance by the amount passed over
+             */
             throw new NotImplementedException();
         }
 
         public void Withdraw(double amount)
         {
+            /* Requirements
+                1.  Throw the exception if your current account doesn't exist
+                2.  Subtract the amount from your current balance
+             */
             throw new NotImplementedException();
         }
     }
--- TDDExample/ViewModels/MyAccountPageModel.cs
+++ TDDExample/ViewModels/MyAccountPageModel.cs
@@ -16,22 +16,41 @@ namespace TDDExample
 
         public BankAccount MyAccount { get; set; }
 
+        public double DepositAmount { get; set; }
+
+        public double WithdrawAmount { get; set; }
+
         public ICommand DepositCommand => new Command(DoDeposit);
 
         public ICommand WithdrawCommand => new Command(DoWithDraw);
 
         private void CreateAccount()
         {
+            /* Requirements
+                1.  Create the account with your name and initial deposit when the page is initiated
+                2.  Set MyAccount
+                3.  Wrap with try & catch statement in case the exception is thrown
+             */
             // TODO: Implements
         }
 
         private void DoDeposit()
         {
+            /* Requirements
+                1.  Call the Deposit method from IBankAccountService when DepositAmount is greater than 0
+                2.  Update the Balance property of MyAccount
+                3.  Wrap with try & catch statement in case the exception is thrown
+             */
             // TODO: Implements
         }
 
         private void DoWithDraw()
         {
+            /* Requirements
+                1.  Call the Withdraw method from IBankAccountService when WithdrawAmount is greater than 0
+                2.  Update the Balance property of MyAccount
+                3.  Wrap with try & catch statement in case the exception is thrown
+             */
             // TODO: Implements
         }
     }
