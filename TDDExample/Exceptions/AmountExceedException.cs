2bea4484731f1d83521e1fee1e6c9714545464a7
Jay <jay.sim@xam-consulting.com>
2021-09-29 10:52:31 +1000

    amend the namespace

--- TDDExample/Exceptions/AmountExceedException.cs
+++ TDDExample/Exceptions/AmountExceedException.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 
-namespace TDDExample.Exceptions
+namespace TDDExample
 {
     [ExcludeFromCodeCoverage]
     public class AmountExceedException : Exception
